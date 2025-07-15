7ceddaf70b7ad26f31278beed022c8d54432165d
Talic72 <EMORRISWORK72@GMAIL.COM>
2025-07-14 23:35:48 -0400

    finis

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+
+        String play = "Y";
+
+        while (play.equalsIgnoreCase("Y")) {
+            int dice1 = rnd.nextInt(6) + 1;
+            int dice2 = rnd.nextInt(6) + 1;
+            int totaldie = dice1 + dice2;
+            int point = 0;
+
+            System.out.println("First dice: " + dice1);
+            System.out.println("Second dice: " + dice2);
+            System.out.println("Total dice roll: " + totaldie);
+
+            if (totaldie == 2 || totaldie == 3 || totaldie == 12) {
+                System.out.println(totaldie + " is a craps number. You lose!");
+            } else if (totaldie == 7 || totaldie == 11) {
+                System.out.println(totaldie + " is a natural number. You Win!");
+            } else {
+                point = totaldie;
+                System.out.println(totaldie + " is now the point number.");
+
+                boolean over = false;
+                while (!over) {
+                    dice1 = rnd.nextInt(6) + 1;
+                    dice2 = rnd.nextInt(6) + 1;
+                    totaldie = dice1 + dice2;
+
+                    System.out.println("First dice: " + dice1);
+                    System.out.println("Second dice: " + dice2);
+                    System.out.println("Total dice roll: " + totaldie);
+                    System.out.println("Trying for point...");
+
+                    if (totaldie == point) {
+                        System.out.println("You made the point! You Win!");
+                        over = true;
+                    } else if (totaldie == 7) {
+                        System.out.println("You rolled a 7! You lose.");
+                        over = true;
+                    }
+                }
+            }
+
+            System.out.print("Do you want to play again? (Y/N): ");
+            play = in.nextLine();
         }
+
+        System.out.println("Thanks for playing!");
     }
 }
